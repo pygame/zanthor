f037f4a6507b9ca48a5366d7c4499847d002ee8c
renesd <renesd@3fdca168-0110-4d55-91d0-45d86cb198c5>
2011-03-03 16:58:06 +0000

    changed cyclic list to use list instead of UserList if UserList fails to import

--- trunk/zanthor/const.py
+++ trunk/zanthor/const.py
@@ -22,11 +22,11 @@ def data_dir(*args):
 
 # the base size.
 BSW,BSH = 640,480
-SW,SH = 640,480
 SW,SH = 320,200
 SW,SH = 800,600
 SW,SH = 1280,800
 SW,SH = 1024,768
+SW,SH = 640,480
 
 DX = float(SW) / BSW
 DY = float(SH) / BSH
